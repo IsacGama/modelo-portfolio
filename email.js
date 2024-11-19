e04027ba5025b4dd48298b99cba6267c851f10e6
IsacGama <diasgamaisac0@gmail.com>
2024-11-19 20:57:55 -0300

    .

--- email.js
+++ email.js
@@ -1,16 +1,19 @@
-async function sendEmail(formData) {
-    const response = await fetch('/api/sendEmail', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ formData }),
-    });
+emailjs.init(process.env.USER_ID);
 
-    if (!response.ok) {
-        throw new Error('Erro ao enviar o e-mail.');
-    }
+document.getElementById("contact-form").addEventListener("submit", function(event) {
+    event.preventDefault();
 
-    const data = await response.json();
-    console.log(data.message);
-} 
+    const form = document.getElementById("contact-form");
+
+    // Envie o formulário usando emailjs.sendForm
+    emailjs.sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, form)
+        .then((response) => {
+            console.log("E-mail enviado com sucesso!", response);
+            alert("E-mail enviado com sucesso!");
+            form.reset();
+        })
+        .catch((error) => {
+            console.error("Erro ao enviar o e-mail:", error);
+            alert("Erro ao enviar o e-mail: " + error.text);
+        });
+});
