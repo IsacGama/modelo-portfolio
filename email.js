ef808229da33f2051f300c20153353dd6e8890e5
IsacGama <diasgamaisac0@gmail.com>
2024-11-19 20:18:54 -0300

    a

--- email.js
+++ email.js
@@ -1,17 +1,15 @@
-emailjs.init(process.env.USER_ID); // Substitua com seu User ID fornecido pelo EmailJS
+emailjs.init(process.env.USER_ID); 
 
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
 
-    // Obtenha o elemento do formulário
     const form = document.getElementById("contact-form");
 
-    // Envie o formulário usando emailjs.sendForm
     emailjs.sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, form)
         .then((response) => {
             console.log("E-mail enviado com sucesso!", response);
             alert("E-mail enviado com sucesso!");
-            form.reset(); // Reseta o formulário após envio bem-sucedido
+            form.reset(); 
         })
         .catch((error) => {
             console.error("Erro ao enviar o e-mail:", error);
