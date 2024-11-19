0e9c03b3b30e6cb8f62aba9ff230fc5f7556b420
IsacGama <diasgamaisac0@gmail.com>
2024-11-19 20:24:04 -0300

    .

--- email.js
+++ email.js
@@ -1,11 +1,11 @@
-emailjs.init(process.env.USER_ID);
+emailjs.init(process.env.NEXT_PUBLIC_USER_ID);
 
 document.getElementById("contact-form").addEventListener("submit", function(event) {
     event.preventDefault();
 
     const form = document.getElementById("contact-form");
 
-    emailjs.sendForm(process.env.SERVICE_ID, process.env.TEMPLATE_ID, form)
+    emailjs.sendForm(process.env.NEXT_PUBLIC_SERVICE_ID, process.env.NEXT_PUBLIC_TEMPLATE_ID, form)
         .then((response) => {
             console.log("E-mail enviado com sucesso!", response);
             alert("E-mail enviado com sucesso!");
