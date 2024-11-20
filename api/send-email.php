aa53d198477d6c755d7778c0a646782f519edc62
IsacGama <diasgamaisac0@gmail.com>
2024-11-20 15:22:53 -0300

    .

--- api/send-email.php
+++ api/send-email.php
@@ -3,6 +3,8 @@
 header("Access-Control-Allow-Origin: https://modelo-portfolio-psi.vercel.app");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
+header("Content-Type: application/json");
+
 
 if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     http_response_code(200); // Responde a requisições preflight
